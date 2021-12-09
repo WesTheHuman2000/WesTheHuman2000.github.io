2b908ad758f7d7b31409ab40b7b3afdb21d14bff
WesTheHuman2000 <89660042+WesTheHuman2000@users.noreply.github.com>
2021-12-09 18:43:13 -0500

    Create Lin-Activity16.js

---
+++ itis3135/Activity16/Lin-Activity16.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var url = 
+        "http://api.flickr.com/services/feeds/photos_public.gne?" + 
+        "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding"; 
+ 
+    $.getJSON(url, function(data) { 
+        var html = ""; 
+        $.each(data.items, function(i, item){ 
+            html += "<a href=''  data-lightbox='' data-title='' >"+"<img src ="+ item.media.m +">"+"</a>"; 
+            
+    //        // Remove the first paragraph of the description 
+    //        html = html.replace( 
+    //               "<p><a href=\"http://www.flickr.com/people/" + 
+    //               "82407828@N07/\">zakruvalcaba<\/a>" + 
+    //               " posted a photo:<\/p>", ""); 
+        }); 
+        $("#new_building").append(html);
+        $("#new_building").html(html); 
+    }); 
+});
